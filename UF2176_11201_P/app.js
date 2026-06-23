5de9dff9418c51941cbbd7b9ec8e9e3dcec055fe
H.Silva <trindadeproduction@gmail.com>
2026-06-23 13:11:39 +0200

    "Entrega inicial del proyecto UF2176"

---
+++ UF2176_11201_P/app.js
@@ -0,0 +1,67 @@
+<<<<<<< HEAD
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+const profesoresRoutes = require("./routes/profesoresRoutes");
+const alumnosRoutes = require("./routes/alumnosRoutes");
+const cursosRoutes = require("./routes/cursosRoutes");
+const especialidadesRoutes = require("./routes/especialidadesRoutes");
+const matriculasRoutes = require("./routes/matriculasRoutes");
+const consultasRoutes = require("./routes/consultasRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/profesores", profesoresRoutes);
+app.use("/api/alumnos", alumnosRoutes);
+app.use("/api/cursos", cursosRoutes);
+app.use("/api/especialidades", especialidadesRoutes);
+app.use("/api/matriculas", matriculasRoutes);
+app.use("/api/consultas", consultasRoutes);
+app.use('/health', (req, res) => {
+    res.status(200).json({ message: 'API is healthy' });
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en puerto ${PORT}`);
+});
+=======
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+const profesoresRoutes = require("./routes/profesoresRoutes");
+const alumnosRoutes = require("./routes/alumnosRoutes");
+const cursosRoutes = require("./routes/cursosRoutes");
+const especialidadesRoutes = require("./routes/especialidadesRoutes");
+const matriculasRoutes = require("./routes/matriculasRoutes");
+const consultasRoutes = require("./routes/consultasRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/profesores", profesoresRoutes);
+app.use("/api/alumnos", alumnosRoutes);
+app.use("/api/cursos", cursosRoutes);
+app.use("/api/especialidades", especialidadesRoutes);
+app.use("/api/matriculas", matriculasRoutes);
+app.use("/api/consultas", consultasRoutes);
+app.use('/health', (req, res) => {
+    res.status(200).json({ message: 'API is healthy' });
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en puerto ${PORT}`);
+});
+>>>>>>> 39c747a (feat(Models): corrected consultaModels grammar, which generated conflict with bd)
